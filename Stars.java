7b178618604ffb27ee0233bdbd19bc9caacfc7fc
jessicagerson <jessygerson123@gmail.com>
2017-10-23 10:53:51 -0700

    Ch3Day4

--- Stars.java
+++ Stars.java
@@ -6,8 +6,13 @@ public class Stars
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args)
     {
-          bigToSmall();
-          //smallToBig();
+        bigToSmall();
+          
+        System.out.println("How many stars do you want in the longest row?");
+        int maxNumber = scan.nextInt();
+        String it = " ";
+        
+        smallToBig(it, maxNumber);
           //oppositeBigToSmall();
           //diamond();
 
@@ -15,11 +20,30 @@ public class Stars
          public static void bigToSmall(){
         System.out.println("How many stars do you want in the longest row?");
         int maxNum = scan.nextInt();
-        int i;
-            for (i = 0; i <= maxNum; i++){
-            for (i = 0; i<=maxNum; i++){
-                System.out.print("*");
-            }
+        for (int row = maxNum; row >= 0; row--){
+            for (int star = 1; star <= row; star++){
+                 System.out.print("*");
+                }
+            System.out.println();
         }
+    }
+    
+        public static void smallToBig(String it, int maxNumber){
+
+        for (int rows = maxNumber; rows >0 ; rows--){
+            
+                 print(" ", rows );
+                 print("*", (maxNumber - rows));
+                
+                 System.out.println();
+        }
+        
+    }
+    
+    public static void print(String thing, int num) {
+        for(int i = 0; i < num; i++) {
+            System.out.print(thing);
+        }
+        
     }
 }
